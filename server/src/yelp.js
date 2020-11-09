658167ae6cef5f978082f9bb1d4ae3729ec86253
Mohamed Shalaby <ms766@njit.edu>
2020-11-09 03:15:38 +0000

    updated yelp api fixed all the errors and set the proper secret key for the yelp api

--- server/src/yelp.js
+++ server/src/yelp.js
@@ -1,19 +1,19 @@
-console.log(process.env.SECRET_KEY)
+require('dotenv').config()
 
-var axios = require('axios');
+const axios = require('axios')
 
-var config = {
-  method: 'get',
-  url: 'https://api.yelp.com/v3/businesses/search?location=newyork, newyork',
-  headers: { 
-    'Authorization': 'SECRET_KEY'
-  }
-};
+const config = {
+	method: 'get',
+	url: 'https://api.yelp.com/v3/businesses/search?location=newyork, newyork',
+	headers: {
+		Authorization: process.env.YELP_SECRET_KEY
+	},
+}
 
 axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+	.then((response) => {
+		console.log(JSON.stringify(response.data))
+	})
+	.catch((error) => {
+		console.log(error)
+	})
