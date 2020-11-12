0f52cdc9ab96db9e6ecf741e36858ec03789b1e7
ms <ms766@njit.edu>
2020-11-12 15:14:06 -0500

    added darkmode for the header and searchbar

--- client/src/components/DealsHeader.js
+++ client/src/components/DealsHeader.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import {
-	Box, Flex, InputGroup, InputLeftElement, Input, Icon
+	Box, Flex, InputGroup, InputLeftElement, Input, Icon, useColorMode
 } from '@chakra-ui/core'
 import LogoutButton from './LogoutButton'
 import LoginButton from './LoginButton'
@@ -10,18 +10,20 @@ import ToggleColorButton from './ToggeColorButton'
 const Header = () => {
 	const userContext = useContext(UserContext)
 
+	const { colorMode } = useColorMode()
+
 	return (
 		<Box
 			pos="fixed"
 			as="header"
 			top="0"
 			zIndex="4"
-			bg="white"
 			left="0"
 			right="0"
 			borderBottomWidth="1px"
 			width="full"
 			height="4rem"
+			bg={colorMode === 'light' ? 'white' : 'gray.700'}
 		>
 			<Box width="full" mx="auto" px={6} pr={[1, 6]} height="100%">
 				<Flex size="100%" p={[0, 6]} pl={[0, 4]} align="center" justify="space-between">
@@ -35,7 +37,7 @@ const Header = () => {
 						<Input
 							type="text"
 							placeholder="Search for deals"
-							bg="gray.100"
+							bg={colorMode === 'light' ? 'gray.100' : 'gray.700'}
 						/>
 					</InputGroup>
 					{userContext.state.isAuthenticated ? <LogoutButton /> : <LoginButton /> }
