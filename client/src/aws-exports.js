ff9f468426d28ae1287ad5ddf2495d59d4e5313c
kandarpajvalia <kandarpa9@gmail.com>
2020-11-13 12:40:08 -0500

    updated auth redirect uri to allow heroku

--- client/src/aws-exports.js
+++ client/src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3000/,https://tabledeals-mvp.herokuapp.com/",
+        "redirectSignOut": "http://localhost:3000/,https://tabledeals-mvp.herokuapp.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_AND_IDENTITY_POOLS"
