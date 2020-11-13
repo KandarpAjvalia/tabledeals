b7f8c50bc0a25656ca7cac56cc408cf24beb8c80
kandarpajvalia <kandarpa9@gmail.com>
2020-11-13 18:14:16 -0500

    fixed dark mode issue for detail deal page

--- client/src/components/DetailedDealCard.js
+++ client/src/components/DetailedDealCard.js
@@ -6,6 +6,7 @@ import {
 	Text,
 	Flex,
 	Stack,
+	useColorMode
 }
 	from '@chakra-ui/core'
 import AddVote from './AddVote'
@@ -23,72 +24,76 @@ const DetailedDealCard = ({
 	zip,
 	openingTime,
 	closingTime
-}) => (
-	<Box
-		borderWidth="1px"
-		borderRadius={8}
-		p={1}
-		mb={2}
-		backgroundColor="white"
-		boxShadow="sm"
-	>
-		<Flex>
-			<Stack ml={3} mt={2} mb={2} w="100%" pr={4}>
-				<Flex align="center" justify="space-between">
+}) => {
+	const { colorMode } = useColorMode()
 
-					<Stack>
-						<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
-							{resName}
-						</Text>
-						<Text fontSize="md" fontWeight="semibold" lineHeight="short">
-							{description}
-						</Text>
-					</Stack>
-					<Stack>
-						<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
-							Open from:
-						</Text>
-						<Text>
-							{openingTime}
-							-
-							{closingTime}
-						</Text>
-					</Stack>
+	return (
+		<Box
+			borderWidth="1px"
+			borderRadius={8}
+			p={1}
+			mb={2}
+			bg={colorMode === 'light' ? 'white' : 'gray.700'}
+			boxShadow="sm"
+		>
+			<Flex>
+				<Stack ml={3} mt={2} mb={2} w="100%" pr={4}>
+					<Flex align="center" justify="space-between">
 
-				</Flex>
-				<Flex>
-					<Stack align="center" ml={2}>
-						<AddVote
-							dealId={dealId}
-						/>
-					</Stack>
-					<Stack ml={3} mt={2} mb={2} w="100%" pr={4}>
-						<Flex align="baseline">
-							<Badge variantColor="orange">{dealType}</Badge>
-						</Flex>
-						<Flex align="center" justify="space-between">
+						<Stack>
 							<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
-								{title}
+								{resName}
 							</Text>
-						</Flex>
-					</Stack>
-				</Flex>
-				<Stack>
-					<Text color="gray.400">
-						{street}
-					</Text>
-					<Text color="gray.400">
-						{city}
-						{', '}
-						{state}
-						{' '}
-						{zip}
-					</Text>
-				</Stack>
+							<Text fontSize="md" fontWeight="semibold" lineHeight="short">
+								{description}
+							</Text>
+						</Stack>
+						<Stack>
+							<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
+								Open from:
+							</Text>
+							<Text>
+								{openingTime}
+								-
+								{closingTime}
+							</Text>
+						</Stack>
 
-			</Stack>
-		</Flex>
-	</Box>
-)
+					</Flex>
+					<Flex>
+						<Stack align="center" ml={2}>
+							<AddVote
+								dealId={dealId}
+							/>
+						</Stack>
+						<Stack ml={3} mt={2} mb={2} w="100%" pr={4}>
+							<Flex align="baseline">
+								<Badge variantColor="orange">{dealType}</Badge>
+							</Flex>
+							<Flex align="center" justify="space-between">
+								<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
+									{title}
+								</Text>
+							</Flex>
+						</Stack>
+					</Flex>
+					<Stack>
+						<Text color="gray.400">
+							{street}
+						</Text>
+						<Text color="gray.400">
+							{city}
+							{', '}
+							{state}
+							{' '}
+							{zip}
+						</Text>
+					</Stack>
+
+				</Stack>
+			</Flex>
+		</Box>
+	)
+}
 
 export default DetailedDealCard
