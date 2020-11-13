d15fef5fd5173aa2721246d621f89515f0127b6b
kandarpajvalia <kandarpa9@gmail.com>
2020-11-12 20:19:08 -0500

    using env port for binding to heroku's port

--- server/index.js
+++ server/index.js
@@ -4,12 +4,8 @@ const path = require('path')
 const app = express()
 app.use(express.static(path.join(__dirname, 'dist')))
 
-const port = process.env.SERVER_PORT || 4000
+const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
-	// res.send('hi')
-	// res.send(pa.path.join(__dirname, 'dist'))
-	res.sendFile(path.resolve('index.html'))
-})
+app.get('/', (req, res) => res.sendFile(path.resolve('index.html')))
 
 app.listen(port, () => console.log(`server is listening on ${port}`))
