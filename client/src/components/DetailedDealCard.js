396d54e2f8c9dff4dd10aca69679aaa11b058560
benjam9 <bgr9@njit.edu>
2020-11-13 17:35:44 +0000

    Fix query and display on DetailedDealCard

---
+++ client/src/components/DetailedDealCard.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import {
+	Box,
+	Badge,
+	Text,
+	Flex,
+	Stack,
+	IconButton,
+	Heading,
+	Image
+}
+from '@chakra-ui/core'
+
+const DetailedDealCard = ({
+	title,
+	dealType,
+	resName,
+	description,
+	image_url,
+	street,
+	city,
+	state,
+	zip,
+	opening_time,
+	closing_time
+}) => (
+	<Box
+		borderWidth="1px"
+		borderRadius={8}
+		p={1}
+		mb={2}
+		backgroundColor="white"
+		boxShadow="sm"
+	>
+		<Flex>
+			<Stack ml={3} mt={2} mb={2} w="100%" pr={4}>
+				<Flex align="center" justify="space-between">
+
+					<Stack>
+						<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
+							{resName}
+						</Text>
+						<Text fontSize="md" fontWeight="semibold" lineHeight="short">
+							{description}
+						</Text>
+					</Stack>
+					<Stack>
+						<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
+							Open from:
+						</Text>
+						<Text>
+							{opening_time}
+							-
+							{closing_time}
+						</Text>
+					</Stack>
+
+				</Flex>
+				<Flex>
+			<Stack align="center" ml={2}>
+				<IconButton
+					aria-label="Upvote"
+					icon="chevron-up"
+					size="sm"
+					fontSize="20px"
+					onClick={() => console.log('upvote')}
+					variant="ghost"
+					color="gray.500"
+				/>
+				<Box fontWeight="semibold">7</Box>
+				<IconButton
+					aria-label="Downvote"
+					icon="chevron-down"
+					size="sm"
+					fontSize="20px"
+					onClick={() => console.log('downvote')}
+					variant="ghost"
+					color="gray.500"
+				/>
+			</Stack>
+			<Stack ml={3} mt={2} mb={2} w="100%" pr={4}>
+				<Flex align="baseline">
+					<Badge variantColor="orange">{dealType}</Badge>
+				</Flex>
+				<Flex align="center" justify="space-between">
+					<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
+						{title}
+					</Text>
+				</Flex>
+			</Stack>
+		</Flex>
+				<Stack>
+					<Text color="gray.400">
+						{street}
+					</Text>
+					<Text color="gray.400">
+						{city}
+						{', '}
+						{state}
+						{' '}
+						{zip}
+					</Text>
+				</Stack>
+
+			</Stack>
+		</Flex>
+	</Box>
+)
+
+export default DetailedDealCard
--- client/src/pages/DealInfo.js
+++ client/src/pages/DealInfo.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useQuery, gql } from '@apollo/client'
 import Text from '@chakra-ui/core'
 import PageWrapper from './PageWrapper'
-
+import DetailedDealCard from '../components/DetailedDealCard'
 const DealInfo = ({ id }) => {
 	const GET_DEAL_BY_ID_QUERY = gql`
 	query findDeal($id: uuid!) {
@@ -26,23 +26,47 @@ const DealInfo = ({ id }) => {
 	}
 	
 	`
-	// const dealTitle = ({ dealId }) => {
-	//  const { loading, error, data } = useQuery(GET_DEAL_BY_ID_QUERY, {
-	// 		variables: { id : {id} },
-	// 	})
+	const dealInfo = ({ id }) => {
+		const { loading, error, data } = useQuery(GET_DEAL_BY_ID_QUERY, {
+			variables: { "id" : id },
+		})
+		const [deals, setDeals] = useState([])
+
+		useEffect(() => {
+		if (data && data.deal) {
+			setDeals(data.deal)
+			}
+		}, [data])
+		if (loading) return null
+		if (error) return `Error! ${error}`
 		
-	// 	console.log(data)
-	// 	if (loading) return null
-	// 	if (error) return `Error! ${error}`
-	// 	return (
-	// 	 <Text>{data.deal.title}</Text>
-	// 	);
-	// }
+		return (
+			<PageWrapper width="full" maxWidth="1280px" mx="auto" px={6} py={6}>
+			{deals && deals.map((deal) => {
+				const { name, city, state, description, image_url, street, zip, opening_time, closing_time } = deal.restaurant
+				return (
+						<DetailedDealCard
+							key={deal.id}
+							title={deal.title}
+							dealType={deal.type}
+							resName={name}
+							city={city}
+							state={state}
+							description={description}
+							image_url={image_url}
+							street={street}
+							zip={zip}
+							opening_time={opening_time}
+							closing_time={closing_time}
+						/>
+				)
+			})}
+		</PageWrapper>
+		)
+	}
 
 	return (
-		<PageWrapper width="full" maxWidth="1280px" mx="auto" px={6} py={6}>
-			{id}
-		</PageWrapper>
+			<div>{dealInfo({id})}</div>
 	)
 }
 export default DealInfo
