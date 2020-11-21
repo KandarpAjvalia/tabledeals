2cc7bdff2dd43c0afc0608cb9584b351753266ef
Gerry Agbay <gerryagbay@Gerrys-iMac.fios-router.home>
2020-11-21 00:34:51 -0500

    Display user id and deal id to console when star button is clicked

--- client/src/components/AddBookmark.js
+++ client/src/components/AddBookmark.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {
+	useContext,
+} from 'react'
 import { IconButton } from '@chakra-ui/core'
+import { Context as UserContext } from '../context/UserContext'
 
 const AddBookmark = ({ dealId }) => {
+	const userContext = useContext(UserContext)
+	const userId = userContext.state.user && userContext.state.user.sub
+	const { isAuthenticated } = userContext.state
+
 	const onBookmark = () => {
 		console.log('Bookmark button was clicked testing')
+		console.log(userId)
+		console.log(dealId)
 	}
 	return (
 		<>
