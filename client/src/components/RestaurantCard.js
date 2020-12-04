1631302327389c62e5102918d8afa582d077a9e3
kandarpajvalia <kandarpa9@gmail.com>
2020-12-03 23:10:57 -0500

    added util function to format time strings, fixed deal detail and restaurant times

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {
 	Box, Image, useColorMode, Text, Badge, AspectRatioBox
 } from '@chakra-ui/core'
+import { formatTime } from '../utils'
 
 const RestaurantCard = ({
 	// eslint-disable-next-line react/prop-types
@@ -10,12 +11,12 @@ const RestaurantCard = ({
 	const { colorMode } = useColorMode()
 
 	return (
-		<Box maxW="sm" boxShadow="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" bg={colorMode === 'light' ? 'white' : 'gray.700'}>
+		<Box maxW="sm" boxShadow="md" borderWidth="1px" borderRadius="lg" overflow="hidden" bg={colorMode === 'light' ? 'white' : 'gray.700'}>
 			<AspectRatioBox w="100%" maxWidth="400px" maxHeight="275px">
 				<Image src={image} alt={name} objectFit="cover" />
 			</AspectRatioBox>
 
-			<Box p="6">
+			<Box p="3">
 				<Box d="flex" alignItems="baseline">
 					<Text fontSize="xl" fontWeight="semibold" lineHeight="short">
 						{name}
@@ -25,17 +26,17 @@ const RestaurantCard = ({
 					{restaurantDescription}
 				</Badge>
 				<Box>
-					<Text> </Text>
-					<Text fontSize="s" fontWeight="normal" lineHeight="short" color="gray.700">
-						Open:
-						{' '}
-						{openingTime}
-						{' - '}
-						{closingTime}
+					<Text
+						fontSize="s"
+						fontWeight="normal"
+						lineHeight="short"
+						color={colorMode === 'light' ? 'gray.700' : 'gray.400'}
+					>
+						{`${formatTime(openingTime)} - ${formatTime(closingTime)}`}
 					</Text>
 				</Box>
 				<Box>
-					<Text color="gray.600">
+					<Text >
 						{street}
 						{' '}
 						{city}
