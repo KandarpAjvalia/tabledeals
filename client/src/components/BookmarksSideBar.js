3136b3df6fdaa3b3989a865edda6786599551c67
Gerry Agbay <gerryagbay@Gerrys-iMac.fios-router.home>
2020-12-04 03:07:47 -0500

    Add sidebar for bookmarks page

---
+++ client/src/components/BookmarksSideBar.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+	Stack, Text, Box, Flex, useColorMode
+} from '@chakra-ui/core'
+import { Link } from 'react-router-dom'
+
+// eslint-disable-next-line react/prop-types
+const SideBarLink = ({ href, children, icon }) => (
+	<Link to={href}>
+		<Flex align="center" p={1}>
+			<Box as={icon} mr={3} w="24px" />
+			<Text fontWeight="bold">{children}</Text>
+		</Flex>
+	</Link>
+)
+
+const PageLinks = () => (
+	<Stack spacing={0} mb={8}>
+		<SideBarLink href="/">
+			Home
+		</SideBarLink>
+		<SideBarLink href="/deals">
+			Deals
+		</SideBarLink>
+		<SideBarLink href="/restaurants">
+			Restaurants
+		</SideBarLink>
+		<SideBarLink href="/bookmarks">
+			Bookmarks
+		</SideBarLink>
+	</Stack>
+)
+
+const BookmarksSideBar = (props) => {
+	const { colorMode } = useColorMode()
+
+	return (
+		<Box
+			backgroundColor={colorMode === 'light' ? 'white' : 'gray.700'}
+			position="fixed"
+			left="0"
+			width="100%"
+			height="100%"
+			top="0"
+			right="0"
+			// eslint-disable-next-line react/jsx-props-no-spreading
+			{...props}
+		>
+			<Box top="4rem" position="relative" overflowY="auto" borderRightWidth="1px">
+				<Box>
+					<Flex justify="space-between" direction="column" height="calc(100vh - 4rem)" fontSize="sm" p="6">
+						<PageLinks />
+					</Flex>
+				</Box>
+			</Box>
+		</Box>
+	)
+}
+
+export default BookmarksSideBar
