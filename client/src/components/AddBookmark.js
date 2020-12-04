52fd3d67df6caa53255f270a08e34a07287e8098
Gerry Agbay <gerryagbay@Gerrys-iMac.fios-router.home>
2020-12-04 01:01:12 -0500

    clicking the bookmark/star button console logs the userId and dealId

--- client/src/components/AddBookmark.js
+++ client/src/components/AddBookmark.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {
+	useContext,
+} from 'react'
 import { IconButton } from '@chakra-ui/core'
+import { Context as UserContext } from '../context/UserContext'
 
-const AddBookmark = () => (
-	<>
-		<IconButton
-			aria-label="Upvote"
-			icon="star"
-			size="sm"
-			fontSize="20px"
-			variant="ghost"
-			color="gray.400"
-		/>
-	</>
-)
+// eslint-disable-next-line react/prop-types
+const AddBookmark = ({ dealId }) => {
+	const userContext = useContext(UserContext)
+	const userId = userContext.state.user && userContext.state.user.sub
+	const onBookmark = () => {
+		console.log('Bookmark button was clicked testing')
+		console.log(userId)
+		console.log(dealId)
+	}
+	return (
+		<>
+			<IconButton
+				aria-label="Upvote"
+				icon="star"
+				size="sm"
+				fontSize="20px"
+				onClick={() => onBookmark()}
+				variant="ghost"
+				color="gray.400"
+			/>
+		</>
+	)
+}
 export default AddBookmark
