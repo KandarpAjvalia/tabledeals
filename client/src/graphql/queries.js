c308c0e50f55a421118e6ea3ddbb53deac84f498
ms <ms766@njit.edu>
2020-12-06 22:01:20 -0500

    get restaurant data from graphql and display restaurant as map marker

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { gql } from '@apollo/client'
+
 export const GET_DEALS_QUERY = gql`
     query getDeals {
 		deal {
--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,30 +1,38 @@
 import React from 'react'
 import { Marker } from 'react-map-gl'
+import { useQuery } from '@apollo/client'
 import MapLocations from '../components/MapLocations'
 import RestaurantsPageWrapper from './RestaurantsPageWrapper'
-
-const Map = () => (
-	<RestaurantsPageWrapper
-		width="full"
-		maxWidth="1280px"
-		mx="auto"
-		px={6}
-		py={6}
-		isDisabled
-	>
-		<MapLocations>
-			<Marker
-				latitude={40.7357}
-				longitude={-74.1724}
-			>
-				<img
-					src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Map_pin_icon.svg"
-					alt="marker"
-					width="25px"
-				/>
-			</Marker>
-		</MapLocations>
-	</RestaurantsPageWrapper>
-)
-
+import { GET_RESTAURANTS_QUERY } from '../graphql/queries'
+const Map = () => {
+	const { data } = useQuery(GET_RESTAURANTS_QUERY)
+	return (
+		<RestaurantsPageWrapper
+			width="full"
+			maxWidth="1280px"
+			mx="auto"
+			px={6}
+			py={6}
+			isDisabled
+		>
+			<MapLocations>
+				{data && data.restaurant.map((
+					{ lat, lon }
+				) => (
+					<Marker
+						key={lat}
+						latitude={lat}
+						longitude={lon}
+					>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Map_pin_icon.svg"
+							alt="marker"
+							width="25px"
+						/>
+					</Marker>
+				))}
+			</MapLocations>
+		</RestaurantsPageWrapper>
+	)
+}
 export default Map
