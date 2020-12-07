31906f7381bb8e47ca3ce02debf1fa60486f8d74
ms <ms766@njit.edu>
2020-12-06 20:22:16 -0500

    update darkmode

--- client/src/components/MapLocations.js
+++ client/src/components/MapLocations.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react'
+import { useColorMode } from '@chakra-ui/core'
 import ReactMapGL from 'react-map-gl'
-const MapLocations = () => {
+const mapVariants = {
+	light: 'mapbox/streets-v11',
+	dark: 'kajvalia/ckidsm04v2z651apgywfo3dhp'
+}
+// eslint-disable-next-line react/prop-types
+const MapLocations = ({ children }) => {
+	const { colorMode } = useColorMode()
 	const [viewport, setViewport] = useState({
 		latitude: 40.7357,
 		longitude: -74.1724,
@@ -9,17 +16,17 @@ const MapLocations = () => {
 		pitch: 0,
 	})
 	return (
-		<div>
-			<ReactMapGL
-				// eslint-disable-next-line react/jsx-props-no-spreading
-				{...viewport}
-				width="1000px"
-				height="700px"
-				mapStyle="mapbox://styles/mapbox/streets-v11"
-				onViewportChange={(nextViewport) => setViewport(nextViewport)}
-				mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-			/>
-		</div>
+		<ReactMapGL
+			// eslint-disable-next-line react/jsx-props-no-spreading
+			{...viewport}
+			width="1000px"
+			height="700px"
+			mapStyle={`mapbox://styles/${mapVariants[colorMode]}`}
+			onViewportChange={(nextViewport) => setViewport(nextViewport)}
+			mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+		>
+			{children}
+		</ReactMapGL>
 	)
 }
 export default MapLocations
